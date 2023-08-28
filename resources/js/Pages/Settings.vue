d091f1f7296be72bdc6f60f760249dc797b9ddcc
rheannemcintosh <rheannemcintosh@gmail.com>
2023-08-28 23:56:36 +0100

    Create a settings and a users page

---
+++ resources/js/Pages/Settings.vue
@@ -0,0 +1,9 @@
+<script>
+    export default {}
+</script>
+
+<template>
+    <h1>Settings</h1>
+</template>
+
+
---
+++ resources/js/Pages/Users.vue
@@ -0,0 +1,9 @@
+<script>
+    export default {}
+</script>
+
+<template>
+    <h1>Users</h1>
+</template>
+
+
