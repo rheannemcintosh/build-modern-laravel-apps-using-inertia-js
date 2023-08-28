cc72e7e73100a8f239b3be4efdfb171f8621ae7e
rheannemcintosh <rheannemcintosh@gmail.com>
2023-08-29 00:12:13 +0100

    Create navigation using Inertia

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -1,9 +1,26 @@
-<script>
-    export default {}
+<script setup>
+    import { Link } from '@inertiajs/vue3'
 </script>
 
 <template>
     <h1>Settings</h1>
+
+
+    <nav>
+        <ul>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+
+            <li>
+                <Link href="/users">Users</Link>
+            </li>
+
+            <li>
+                <Link href="/settings">Settings</Link>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 
