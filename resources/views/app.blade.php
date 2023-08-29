44a91971600fb8052aa688a1529856c961d49c5b
rheannemcintosh <rheannemcintosh@gmail.com>
2023-08-30 00:23:24 +0100

    Add tailwind

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         @vite('resources/js/app.js')
         @inertiaHead
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
 
     <body>
