4dfe52f046ff5e359459be4376ad7b7a3cd3005c
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-01 00:31:27 +0100

    Add conditional styling if current page is selected

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -6,13 +6,13 @@
     <nav class="mt-6">
         <ul class="list-disc">
             <li>
-                <Link href="/" class="text-blue-500 hover:underline">Home</Link>
+                <Link href="/" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.url === '/'}">Home</Link>
             </li>
             <li>
-                <Link href="/users" class="text-blue-500 hover:underline">Users</Link>
+                <Link href="/users" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.url === '/users'}">Users</Link>
             </li>
             <li>
-                <Link href="/settings" class="text-blue-500 hover:underline">Settings</Link>
+                <Link href="/settings" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.url === '/settings'}">Settings</Link>
             </li>
             <li>
                 <Link href="/logout" method="post" as="button" class="text-blue-500 hover:underline">Log Out</Link>
