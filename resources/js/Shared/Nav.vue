1f8580cb81cbce3c38ada4f3e8c9aa6966f43f49
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-01 00:34:07 +0100

    Use page component instead

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -6,13 +6,13 @@
     <nav class="mt-6">
         <ul class="list-disc">
             <li>
-                <Link href="/" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.url === '/'}">Home</Link>
+                <Link href="/" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.component === 'Home'}">Home</Link>
             </li>
             <li>
-                <Link href="/users" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.url.startsWith('/users')}">Users</Link>
+                <Link href="/users" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.component === 'Users'}">Users</Link>
             </li>
             <li>
-                <Link href="/settings" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.url.startsWith('/settings')}">Settings</Link>
+                <Link href="/settings" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.url.component === 'Settings'}">Settings</Link>
             </li>
             <li>
                 <Link href="/logout" method="post" as="button" class="text-blue-500 hover:underline">Log Out</Link>
