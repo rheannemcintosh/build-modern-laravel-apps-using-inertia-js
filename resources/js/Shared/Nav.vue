eaf3a30700882d563cecfa59dadc2b52c393dcea
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-01 00:46:02 +0100

    Use NavLinks in the Nav component

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,18 +1,34 @@
 <script setup>
     import { Link } from '@inertiajs/vue3'
+    import NavLink from './NavLink.vue';
 </script>
 
 <template>
     <nav class="mt-6">
         <ul class="list-disc">
             <li>
-                <Link href="/" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.component === 'Home'}">Home</Link>
+                <NavLink
+                    href="/"
+                    :active="$page.component === 'Home'"
+                >
+                    Home
+                </NavLink>
             </li>
             <li>
-                <Link href="/users" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.component === 'Users'}">Users</Link>
+                <NavLink
+                    href="/users"
+                    :active="$page.component === 'Users'"
+                >
+                    Users
+                </NavLink>
             </li>
             <li>
-                <Link href="/settings" class="text-blue-500 hover:underline" :class="{'font-bold underline': $page.url.component === 'Settings'}">Settings</Link>
+                <NavLink
+                    href="/settings"
+                    :active="$page.component === 'Settings'"
+                >
+                    Settings
+                </NavLink>
             </li>
             <li>
                 <Link href="/logout" method="post" as="button" class="text-blue-500 hover:underline">Log Out</Link>
