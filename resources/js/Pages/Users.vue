cc4a146ff83c0c87caaf9635374bfb68e930852c
rheannemcintosh <rheannemcintosh@gmail.com>
2023-08-31 01:20:23 +0100

    Add link which preserves scroll

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,5 +1,6 @@
 <script setup>
     import Nav from '../Shared/Nav.vue';
+    import { Link } from '@inertiajs/vue3'
 
     const props = defineProps({
         time: String,
@@ -9,4 +10,11 @@
 <template>
     <h1 class="text-4xl font-bold">Users</h1>
     <Nav />
+
+    <div style="margin-top: 900px">
+        <p>The current time is {{ time}}.</p>
+        <Link href="/users" class="text-blue-500" preserve-scroll>
+            Refresh
+        </Link>
+    </div>
 </template>
