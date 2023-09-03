ac8482b04e8df6a83bb58c439c2af7e7058135c6
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-03 23:33:50 +0100

    Globally register the link component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
 
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import {createInertiaApp, Link} from '@inertiajs/vue3'
 
 createInertiaApp({
     resolve: name => {
@@ -11,6 +11,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .component('Link', Link)
             .mount(el)
     },
     progress: {
