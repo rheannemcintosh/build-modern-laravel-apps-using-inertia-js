9d0ba32a52d533aeecc925f08d31c2b952ae9ef7
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-04 21:38:43 +0100

    Add meta description

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,6 +5,7 @@
 <template>
     <Head>
         <title>My App - Home</title>
+        <meta type="description" contents="Home Information" head-key="description">
     </Head>
     <h1 class="text-3xl">Home</h1>
 </template>
