11f945eaaef745178806ac2a054d649ef0ed04fd
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-06 23:14:20 +0100

    Display list of users in new table

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -9,24 +9,31 @@
         <title>Users</title>
     </Head>
 
+    <h1 class="text-3xl">Users</h1>
+
     <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                     <table class="min-w-full divide-y divide-gray-200">
                         <tbody class="bg-white divide-y divide-gray-200">
-                            <tr>
+                            <tr v-for="user in users" :key="user.id">
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <div class="flex items-center">
                                         <div>
                                             <div class="text-sm font-medium text-gray-900">
-                                                Jane Cooper
+                                                {{ user.name }}
                                             </div>
                                         </div>
                                     </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                    <Link
+                                        :href="`/users/${user.id}/edit`"
+                                        class="text-indigo-600 hover:text-indigo-900"
+                                    >
+                                        Edit
+                                    </Link>
                                 </td>
                             </tr>
                         </tbody>
@@ -35,15 +42,4 @@
             </div>
         </div>
     </div>
-
-    <h1 class="text-3xl">Users</h1>
-
-    <ul>
-        <li
-            v-for="user in users"
-            :key="user.id"
-            v-text="user.name"
-        />
-    </ul>
-
 </template>
