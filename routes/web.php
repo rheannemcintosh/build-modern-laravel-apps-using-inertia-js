137d392f97469c9e78ee6ac33b2ac81fc5f8fdd0
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-08 15:47:50 +0100

    Update the user routes

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 
 
 Route::get('/users', function () {
-    return Inertia::render('Users', [
+    return Inertia::render('Users/Index', [
         'users' => User::query()
             ->when(Request::input('search'), function ($query, $search) {
                 $query->where('name', 'like', "%{$search}%");
@@ -34,6 +34,10 @@ Route::get('/users', function () {
             ]),
         'filters' => Request::only(['search'])
     ]);
+});
+
+Route::get('/users/create', function () {
+    return Inertia::render('Users/Create');
 });
 
 Route::get('/settings', function () {
