171cd6bcc6d09c6af5eaf1780e14ea5526f7057e
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-15 14:50:39 +0100

    Use splitVendorChunkPlugin to create a vendor js file

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import {defineConfig, splitVendorChunkPlugin} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url'
@@ -10,6 +10,7 @@ export default defineConfig({
             input: ['resources/js/app.js'],
             refresh: true,
         }),
+        splitVendorChunkPlugin(),
     ],
     resolve: {
         alias: {
