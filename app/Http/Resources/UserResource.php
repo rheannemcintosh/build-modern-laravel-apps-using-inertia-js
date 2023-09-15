c806f6a3dfde27ec2615c064ce2a21f932343ab3
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-15 16:02:02 +0100

    Populate a user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class UserResource extends JsonResource
 {
@@ -14,6 +15,13 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->when(Auth::user()->is($this->resource), $this->email),
+            'can' => [
+                'update' => Auth::user()->can('update', $this->resource)
+            ]
+        ];
     }
 }
