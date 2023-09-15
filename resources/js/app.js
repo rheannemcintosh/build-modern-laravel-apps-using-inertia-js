d4583e6452b73cc83bc911a828f4054bdd38fa9a
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-15 14:51:10 +0100

    Split app.js into separate page .js files

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,17 +4,23 @@ import '../css/app.css';
 import { createApp, h } from 'vue'
 import {createInertiaApp, Link, Head} from '@inertiajs/vue3'
 import Layout from "./Shared/Layout.vue";
+import {resolvePageComponent} from "laravel-vite-plugin/inertia-helpers";
 
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        let page = pages[`./Pages/${name}.vue`]
+    resolve: async name => {
+
+        const page = (
+            await resolvePageComponent(
+                `./Pages/${name}.vue`,
+                import.meta.glob('./Pages/**/*.vue')
+            )
+        );
 
         if (page.default.layout === undefined) {
             page.default.layout = Layout;
         }
 
-        return pages[`./Pages/${name}.vue`]
+        return page;
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
