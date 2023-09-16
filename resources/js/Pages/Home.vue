2e4175b61457f3a44c45147b5f99fc908185b6b4
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-16 21:19:36 +0100

    Use highlight.js for a coding blog

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,4 +1,18 @@
-<script setup>
+<script>
+import hljs from "highlight.js/lib/common";
+import php from "highlight.js/lib/languages/php.js";
+import javascript from "highlight.js/lib/languages/javascript.js";
+import "highlight.js/styles/github-dark.css";
+
+hljs.registerLanguage("php", php);
+hljs.registerLanguage("javascript", javascript);
+
+export default {
+    mounted() {
+        hljs.highlightAll();
+    }
+}
+
 </script>
 
 <template>
@@ -6,5 +20,32 @@
         <title>Home</title>
         <meta type="description" contents="Home Information" head-key="description">
     </Head>
+
     <h1 class="text-3xl">Home</h1>
+
+    <pre>
+        <code>
+            class Example
+            {
+                public function __construct()
+                {
+                    //
+                }
+            }
+        </code>
+    </pre>
+
+    <br>
+
+    <pre>
+        <code>
+            class ExampleTwo
+            {
+                public function __construct()
+                {
+                    //
+                }
+            }
+        </code>
+    </pre>
 </template>
