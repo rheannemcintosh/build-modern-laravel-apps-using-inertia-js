0e7b349d93c2eedd42209350cbc9bc3c82ecd6c1
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-17 16:44:37 +0100

    Allow users to copy a piece of code

--- resources/js/Components/Highlight.vue
+++ resources/js/Components/Highlight.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <header class="bg-gray-800 text-white flex justify-end px-2 py-1 text-xs border-b border-gray-700">
-            <button class="hover:bg-gray-600 rounded px-2">Copy</button>
+            <button class="hover:bg-gray-600 rounded px-2" @click="copyToClipboard">{{ copied ? 'Copied' : 'Copy' }}</button>
         </header>
         <pre>
             <code ref="block">
@@ -16,11 +16,29 @@
     import { highlightElement } from '@/Services/SyntaxHighlighing.js';
     import { onMounted, ref } from 'vue';
 
-    defineProps({
+    let props = defineProps({
         code: String,
     });
 
     let block = ref(null);
+    let copied = ref(false);
+
+    let copyToClipboard = () => {
+        if (navigator && navigator.clipboard) {
+            navigator.clipboard.writeText(props.code);
+
+            copied.value = true;
+
+            setTimeout(() => {
+                copied.value = false
+            }, 3000);
+
+            return;
+        }
+
+        alert('Apologies, your browser does not support the Clipboard API.');
+
+    };
 
     onMounted(() => {
         highlightElement(block.value);
