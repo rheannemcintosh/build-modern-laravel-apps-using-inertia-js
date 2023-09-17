a299c444ca93db51ddab9f82dd4ce97ff2cb2c77
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-17 21:11:16 +0100

    Create a new threads page

---
+++ resources/js/Pages/Threads/Index.vue
@@ -0,0 +1,22 @@
+<script setup>
+    defineProps({
+        threads: Array
+    })
+</script>
+
+<template>
+    <Head title="Threads" />
+
+    <div>
+        <article v-for="thread in threads" :key="thread.id" class="mb-8">
+            <header>
+                <h1 class="font-bold text-lg">{{ thread.title }}</h1>
+                <p class="text-xs">By {{ thread.user_id }}</p>
+            </header>
+
+            <div class="mt-3">
+                {{ thread.body }}
+            </div>
+        </article>
+    </div>
+</template>
--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -23,6 +23,14 @@
             </li>
             <li>
                 <NavLink
+                    href="/threads"
+                    :active="$page.component === 'Threads'"
+                >
+                    Threads
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
                     href="/settings"
                     :active="$page.component === 'Settings'"
                 >
